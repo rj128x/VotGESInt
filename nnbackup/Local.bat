96550b6948758c13a22bdf3c21238c2cc8a21b4a
rj128x <rj128x@gmail.com>
2012-09-13 13:08:54 +0600

    123

---
+++ nnbackup/Local.bat
@@ -0,0 +1,3 @@
+D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\DBConsole\bin\Release -o D:\run\DB\ -s -e -x *.Data\Settings.xml
+D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\ModbusConsole\bin\Release -o D:\run\MB\ -s -e -x *.Data\Settings.xml
+pause
