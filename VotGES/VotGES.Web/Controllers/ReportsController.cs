3bbbf63c5f87324bb67f0aa47da6d6606a2d4efb
rj128x <rj128x@gmail.com>
2012-09-18 11:31:28 +0600

    123

--- VotGES/VotGES.Web/Controllers/ReportsController.cs
+++ VotGES/VotGES.Web/Controllers/ReportsController.cs
@@ -6,6 +6,12 @@ using System.Web.Mvc;
 using VotGES.Piramida.Report;
 using VotGES.PBR;
 using VotGES.Piramida;
+using System.IO;
+using System.Web.UI;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System.Text;
 
 namespace VotGES.Web.Controllers
 {
@@ -19,14 +25,58 @@ namespace VotGES.Web.Controllers
 		}
 
 		[AcceptVerbs(HttpVerbs.Get)]
-		public ActionResult SutVed(int year, int month, int day) {
-			Logger.Info(String.Format("Суточная ведомость за {0}.{1}.{2}",day,month,year));
+		public ActionResult SutVedPDF(int year, int month, int day) {
+			Logger.Info(String.Format("Суточная ведомость за {0}.{1}.{2}", day, month, year));
 			DateTime date=new DateTime(year, month, day);
 			SutVedReport report=new SutVedReport(date.Date, date.Date.AddDays(1), IntervalReportEnum.halfHour);
 			report.ReadData();
 
-			return View("SutVed", report);
+			StringWriter writer=new StringWriter();
+						
+			ViewResult view=View("SutVedPart", report);
+			view.ExecuteResult(this.ControllerContext);
+			view.View.Render(new ViewContext(this.ControllerContext,view.View,this.ViewData,this.TempData, writer),writer);
+			string html=writer.GetStringBuilder().ToString();
+			writer.Close();
+			
+			MemoryStream outPDF=new MemoryStream();
+			Document document = new Document(PageSize.A4_LANDSCAPE, 0, 0, 0, 0);
+			PdfWriter.GetInstance(document, outPDF);
+			document.Open();
+
+			//html = "<h1>dsgdfg</h1>";
+			List<IElement> parsedHtmlElements = HTMLWorker.ParseToList(new StringReader(html), new StyleSheet());
+						
+
+			foreach (IElement htmlElement in parsedHtmlElements)
+				document.Add(htmlElement as IElement); ;
+			document.Close();
+			//return view;
+
+			System.Web.HttpResponse response = System.Web.HttpContext.Current.Response;
+			response.Clear();
+			response.ContentType = "application/pdf";
+			response.AddHeader("Content-Disposition", string.Format("attachment;filename=sutVed.pdf;size={0}",outPDF.ToArray().Length));
+			response.Flush();
+			response.BinaryWrite(outPDF.ToArray());
+			response.Flush();
+			response.End();
+			return null;
 		}
+
+		[AcceptVerbs(HttpVerbs.Get)]
+		public ActionResult SutVed(int year, int month, int day) {
+			Logger.Info(String.Format("Суточная ведомость за {0}.{1}.{2}", day, month, year));
+			DateTime date=new DateTime(year, month, day);
+			SutVedReport report=new SutVedReport(date.Date, date.Date.AddDays(1), IntervalReportEnum.halfHour);
+			report.ReadData();
+
+			StringWriter writer=new StringWriter();
+
+			ViewResult view=View("SutVed", report);
+			return view;
+		}
+			
 
 		[AcceptVerbs(HttpVerbs.Get)]
 		public ActionResult PBR(int year, int month, int day) {
@@ -40,8 +90,8 @@ namespace VotGES.Web.Controllers
 		public ActionResult Prikaz20(int year, int month, int day) {
 			Logger.Info(String.Format("Приказ 20 за {0}.{1}.{2}", day, month, year));
 			DateTime date=new DateTime(year, month, day);
-			Prikaz20Report report=new Prikaz20Report(date);						
-			
+			Prikaz20Report report=new Prikaz20Report(date);
+
 			return View("Prikaz20", report);
 		}
 
---
+++ VotGES/VotGES.Web/Views/Reports/SutVedPart.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<VotGES.Piramida.Report.SutVedReport>" %>
+<%@ Import Namespace="VotGES.Piramida.Report" %>
+<%@ Import Namespace="VotGES.Piramida" %>
+<%@ Import Namespace="VotGES" %>
+
+<head runat="server">
+    <title>SutVed</title>
+	 <style>
+	 	table,tr,td,p {
+			font-family: 'Arial';
+			font-size: 8pt;
+		}
+		
+		h1,h2,h3,h4,h5,h6,hr
+		{
+			padding:0;
+			margin:0;
+		}
+		
+		
+		h1
+		{
+			font-family: 'Arial';
+			font-size: 10pt;
+		}
+		
+		h2
+		{
+			font-family: 'Arial';
+			font-size: 8pt;
+		}
+		
+	 	table {
+			border-collapse: collapse;		
+		}
+		
+		td, th {
+			border-width: 1px;
+			border-style: solid;
+			border-color: #BBBBFF;
+			padding-left: 3px;
+			padding-right: 3px;
+		}
+		
+		table.cifr td{	
+			text-align: right;
+			white-space: nowrap;
+			padding-left: 1px;
+			padding-right: 1px;
+			width:80px;
+		}
+
+		table.cifr th{
+			text-align: center;			
+			padding-left: 1px;			
+			padding-right: 1px;
+		}
+		
+		
+		table.cifr
+		{
+			margin-top:5px;
+		}
+
+		table td.right,table th.right{
+			text-align: right;
+		}	
+	 </style>
+</head>
+
+<% int[] hours={0,1,5,10,16,19,22}; 
+	//int[] hours= { 6,7,8,9,10};%>
+<body>
+	<table>
+	<tr><th>ываы</th><th>2</th><th>3</th></tr>
+	</table>			
+</body>
