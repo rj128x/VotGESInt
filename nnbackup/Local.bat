e99170bbad1d2d7e186bc3bf0295ec319071d87f
rj128x <rj128x@gmail.com>
2012-09-26 20:14:40 +0600

    123

--- nnbackup/Local.bat
+++
@@ -1,4 +0,0 @@
-D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\DBConsole\bin\Release -o D:\run\DB\ -s -e -x *Data\Settings.xml
-D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\ModbusConsole\bin\Release -o D:\run\MB\ -s -e -x *Data\Settings.xml
-D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\ClearFolder\bin\Release -o D:\run\Clear\ 
-pause
--- nnbackup/Server.bat
+++ nnbackup/Server.bat
@@ -1,4 +1,3 @@
-D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\DBConsole\bin\Release -o Z:\Projects\Modbus\DB\ -s -e -x *Data\Settings.xml
-D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\ModbusConsole\bin\Release -o Z:\Projects\Modbus\MB\ -s -e -x *Data\Settings.xml
+D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\RunModbus -o Z:\Projects\Modbus\RunModbus\ -s -e -x *Data\Settings.xml
 D:\soft\nnBackup\nnbackup sync -i D:\vs2010\VotGESInt\MODBUS\ClearFolder\bin\Release -o Z:\Projects\Modbus\Clear\
 pause
